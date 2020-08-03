3278501d8c4fa7c94f4d8f3261052306ca0a1da8
Alberto del Val Pardo <alberto.delval@bbva.com>
2020-08-03 17:57:47 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.6fd40313889290442b8549aa5dc477be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3467178e416a3c381e791c627588297a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.22beae922b151ebf80fd.bundle.js"
+  },
+  {
+    "url": "runtime~main.22beae922b151ebf80fd.bundle.js"
+  },
+  {
+    "revision": "0db3bf3c850e08652c25",
+    "url": "static/css/main.b576866c.chunk.css"
+  },
+  {
+    "url": "vendors~main.22beae922b151ebf80fd.bundle.js"
+  },
+  {
+    "url": "vendors~main.22beae922b151ebf80fd.bundle.js.LICENSE.txt"
+  }
+]);
